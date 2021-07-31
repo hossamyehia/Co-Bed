a6bb455e9c00572804294d71853e9fbf5dffaebf
hossamyehia <hossamyehia74@yahoo.com>
2021-07-31 21:24:19 +0200

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,10 +1,10 @@
-const username = encodeURIComponent("HossamYahia");
-const password = encodeURIComponent("NoNamegm@1hy200");
+const username = encodeURIComponent("");
+const password = encodeURIComponent("");
 const clusterUrl = "ebda3.eo8hm.mongodb.net";
 
 const authMechanism = "DEFAULT";
 
 module.exports = {
-    'secretKey': 'A5s7-8W9e-Qwe8-L89O',
+    'secretKey': '',
     'mongoUrl' : `mongodb+srv://${username}:${password}@${clusterUrl}/DataBase?retryWrites=true&w=majority`
 }
